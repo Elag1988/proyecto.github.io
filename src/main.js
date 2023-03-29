2e751251c29983c7327971f215fc8b10f35f1e5d
AdrianBulla05 <abullabe@ibero.edu.co>
2023-03-28 22:44:14 -0500

    Se cambia el Main a razón de que el Login se hace sin Bootstrap

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const $btnSignIn= document.querySelector('.btn-sign-in'), 
+$btnSignUp= document.querySelector('.btn-sign-up'),
+$signUp= document.querySelector('.sign-up'),
+$signIn= document.querySelector('.sign-in')
+
+
+    document.addEventListener('click', e => {
+        if (e.target === $btnSignIn || e.target === $btnSignUp) {
+            $signIn.classList.toggle('active');
+            $signUp.classList.toggle('active')
+        }
+    })
